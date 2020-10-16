97e67098cd2789fd8d43eb28556db6502d586116
vsingh1225 <59144363+vsingh1225@users.noreply.github.com>
2020-10-16 00:20:40 -0500

    Add files via upload

---
+++ render_json.py
@@ -0,0 +1,22 @@
+import uuid
+from IPython.display import display_javascript, display_html, display
+import json
+
+class RenderJSON(object):
+    def __init__(self, json_data):
+        if isinstance(json_data, dict):
+            self.json_str = json.dumps(json_data)
+        else:
+            self.json_str = json
+        self.uuid = str(uuid.uuid4())
+        
+    def _ipython_display_(self):
+        display_html('<div id="{}" style="height: 600px; width:100%;"></div>'.format(self.uuid),
+            raw=True
+        )
+        display_javascript("""
+        require(["https://rawgit.com/caldwell/renderjson/master/renderjson.js"], function() {
+          renderjson.set_show_to_level(1)
+          document.getElementById('%s').appendChild(renderjson(%s))
+        });
+        """ % (self.uuid, self.json_str), raw=True)
